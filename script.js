8f890334e2e3136b6c82a18926f69d630a8fb8a6
temielosait <temitope.elodimuor@sait.ca>
2024-11-08 10:14:26 -0700

    add script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+const apiKey = 'MFHh5JqdgRVmnYxuWFFz7pxUdpvPcsJY';
+const endpoint = `https://api.giphy.com/v2/emoji?api_key=MFHh5JqdgRVmnYxuWFFz7pxUdpvPcsJY&limit=100&offset=0`;
+const emojiList = document.getElementById('emoji-list');
